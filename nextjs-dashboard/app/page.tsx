ec977df7c6986ce6a0bf8a6ed07bbc365e943de7
Rei <aloha.reirei1@gmail.com>
2025-03-10 11:02:28 -1000

    Chapter 2 DONE

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,3 +1,5 @@
+import styles from '@/app/ui/home.module.css';
+
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
